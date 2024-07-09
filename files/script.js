874b8167207e2159433c67a8ee7048758fb78e91
MIDHUN V S <114806736+vsmidhun21@users.noreply.github.com>
2024-07-09 21:21:36 +0530

    Initial Test

---
+++ files/script.js
@@ -0,0 +1,58 @@
+/*========== scroll sections active link in navbar ==========*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        console.log(id);
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
+
+
+/*========== Typing animation in home page ==========*/
+var typed = new Typed(".text", {
+    strings: ["Programming" , "Web Development", "Cybersecurity"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+/*========== Go top icon in left bottom ==========*/
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
+
+/*========== Scroll Reveal script ==========*/
+document.addEventListener('DOMContentLoaded', function () {
+    const cards = document.querySelectorAll('.reveal');
+    function checkScroll() {
+    cards.forEach(card => {
+        const rect = card.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+        if (rect.top < windowHeight * 0.75) {
+            card.style.opacity = 1;
+            card.style.transform = 'translateY(0)';
+        }
+    });
+    }
+    checkScroll();
+    window.addEventListener('scroll', checkScroll);
+});
